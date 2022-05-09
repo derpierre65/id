7dad4317a26935cf5286365f75af85fba0d6f694
derpierre65 <git@derpierre65.de>
2022-05-09 12:10:49 +0200

    support laravel 9, check first party client middleware

---
+++ src/Http/Middleware/CheckFirstPartyClient.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace derpierre65\Id\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
+
+class CheckFirstPartyClient
+{
+	public function handle(Request $request, Closure $next, ...$scopes)
+	{
+		if ( !$request->attributes->get('oauth_client_first_party') ) {
+			throw new AuthenticationException('Client is not a first party client.');
+		}
+
+		return $next($request);
+	}
+}
