5eefdbeaa8bc2856fdad538721c5fedf929b621d
derpierre65 <git@derpierre65.de>
2024-02-13 23:36:17 +0000

    fix: missing singleton for Derpierre65Id class

--- src/Providers/ServiceProvider.php
+++ src/Providers/ServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace derpierre65\Id\Providers;
 
+use derpierre65\Id\Derpierre65Id;
+
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
 	public function register()
@@ -10,5 +12,9 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
 			dirname(__DIR__).'/../config/derpierre65-id.php',
 			'derpierre65-id'
 		);
+
+        $this->app->singleton(Derpierre65Id::class, function () {
+            return new Derpierre65Id();
+        });
 	}
 }
